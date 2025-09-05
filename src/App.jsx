145d6377753e3306a26e4402f29c3360e0d911e0
seungju <lsjxcs@gmail.com>
2025-09-06 04:37:46 +0900

    마이페이지 진짜 머지 완료

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import Signup from "./pages/Signup";
 import StudentSignup from "./pages/StudentSignup";
 import TeacherSignup from "./pages/TeacherSignup";
 
-import Question from "./pages/Question";
 import MyPageStud from "./pages/MyPageStud";
 import MyPageUniv from "./pages/MyPageUniv";
 import MentorList from "./pages/MentorList";
@@ -22,11 +21,11 @@ import MentorList from "./pages/MentorList";
 import SelectedMentors from "./pages/SelectedMentors";
 // import Chat from "./pages/Chat";            // 필요 시
 // import Profile from "./pages/Profile";      // 필요 시
-import Question from './pages/Question';
-import Question2 from './pages/Question2';
-import Question3 from './pages/Question3';
-import Question4 from './pages/Question4';
-import QuestionMajor from './pages/QuestionMajor';
+import Question from "./pages/Question";
+import Question2 from "./pages/Question2";
+import Question3 from "./pages/Question3";
+import Question4 from "./pages/Question4";
+import QuestionMajor from "./pages/QuestionMajor";
 
 export default function App() {
   return (
@@ -63,14 +62,26 @@ export default function App() {
             element={<SelectedMentors />}
           />
 
-          <Route caseSensitive={false} path="/question/1" element={<Question />} />
+          <Route
+            caseSensitive={false}
+            path="/question/1"
+            element={<Question />}
+          />
           <Route path="/question/1" element={<Question />} />
           <Route path="/question/2" element={<Question2 />} />
           <Route path="/question/3" element={<Question3 />} />
           <Route path="/question/4" element={<Question4 />} />
           <Route path="/question/major" element={<QuestionMajor />} />
-          <Route caseSensitive={false} path="/mentor-list" element={<MentorList />} />
-          <Route caseSensitive={false} path="/my-mentors" element={<SelectedMentors />} />
+          <Route
+            caseSensitive={false}
+            path="/mentor-list"
+            element={<MentorList />}
+          />
+          <Route
+            caseSensitive={false}
+            path="/my-mentors"
+            element={<SelectedMentors />}
+          />
 
           {/* <Route path="/chat" element={<Chat />} /> */}
           {/* <Route path="/me" element={<Profile />} /> */}
@@ -80,7 +91,6 @@ export default function App() {
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        
       </Routes>
     </BrowserRouter>
   );
